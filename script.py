b4179815a0318f0857bdb5417e7a7a96b2d9247d
John Zimmerman <johnzimmerman@users.noreply.github.com>
2021-06-19 07:37:06 -0500

    Make API URL a config setting vs hardcoded

--- script.py
+++ script.py
@@ -5,12 +5,16 @@ import cbpro
 
 # TODO: If the variables are in the ENV but empty, no warning is given
 # https://www.twilio.com/blog/environment-variables-python
+API_URL = API_URL = environ.get(
+    "CBPRO_API_URL") or "https://api-public.sandbox.pro.coinbase.com"
 API_KEY = environ["CBPRO_KEY"]
 API_SECRET = environ["CBPRO_SECRET"]
 API_PASSPHRASE = environ["CBPRO_PASSPHRASE"]
 
+
 if __name__ == "__main__":
-    auth_client = cbpro.AuthenticatedClient(API_KEY, API_SECRET, API_PASSPHRASE,
-                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    auth_client = cbpro.AuthenticatedClient(
+        API_KEY, API_SECRET, API_PASSPHRASE,
+        api_url=API_URL)
     accounts = auth_client.get_accounts()
     print(accounts)
