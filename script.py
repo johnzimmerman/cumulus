f36bd09c6c574e20a9228a61fa1f01d477e8de0f
John Zimmerman <johnzimmerman@users.noreply.github.com>
2021-06-18 19:23:37 -0500

    Connect to Authenticated Client

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
-print("Hello, world.")
+from os import environ
+
+import cbpro
+
+# TODO: If the variables are in the ENV but empty, no warning is given
+# https://www.twilio.com/blog/environment-variables-python
+API_KEY = environ["CBPRO_KEY"]
+API_SECRET = environ["CBPRO_SECRET"]
+API_PASSPHRASE = environ["CBPRO_PASSPHRASE"]
+
+if __name__ == "__main__":
+    auth_client = cbpro.AuthenticatedClient(API_KEY, API_SECRET, API_PASSPHRASE,
+                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    accounts = auth_client.get_accounts()
+    print(accounts)
