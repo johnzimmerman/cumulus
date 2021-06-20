d13886ce76e580232fa27375f8532573e983c92d
John Zimmerman <johnzimmerman@users.noreply.github.com>
2021-06-19 20:14:12 -0500

    Create OrderManager class and place market

--- cumulus.py
+++ cumulus.py
@@ -10,6 +10,7 @@ import cbpro
 
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
 # TODO: If the variables are in the ENV but empty, no warning is given
 # https://www.twilio.com/blog/environment-variables-python
 API_URL = environ.get(
@@ -19,10 +20,28 @@ API_SECRET = environ["CBPRO_SECRET"]
 API_PASSPHRASE = environ["CBPRO_PASSPHRASE"]
 
 
+class OrderManager:
+    """Manage your orders."""
+
+    def __init__(self, cbpro_auth_client):
+        self.client = cbpro_auth_client
+
+    def placeMarketOrder(self, product_id, amount):
+        logging.info(f"Attempting to purchase {amount} of {product_id}.")
+        # TODO: Ensure amount is correctly formatted
+        response = self.client.place_market_order(
+            product_id=product_id,
+            side='buy',
+            funds=amount)
+        print(response)
+
+
 if __name__ == "__main__":
     logging.info("Running cumulus.py")
     auth_client = cbpro.AuthenticatedClient(
         API_KEY, API_SECRET, API_PASSPHRASE,
         api_url=API_URL)
-    accounts = auth_client.get_accounts()
-    print(accounts)
+    # accounts = auth_client.get_accounts()
+    # print(accounts)
+    my_order_manager = OrderManager(auth_client)
+    my_order_manager.placeMarketOrder("BTC-USD", "100.00")
